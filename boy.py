f2ea7e9fedded84e3dddd1f6f6a0c1fdd00e8482
Hyno03 <jenny2117@naver.com>
2023-10-11 19:44:52 +0900

    Draw boy image

--- boy.py
+++ boy.py
@@ -5,36 +5,34 @@ class Idle:
 
     @staticmethod
     def enter(boy):
-        boy.frame = 0
+        pass
 
     @staticmethod
     def exit(boy):
         pass
+
     @staticmethod
     def do(boy):
-        boy.frame = (boy.frame + 1)% 8
+        boy.frame = (boy.frame + 1) % 8
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw
-
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
 
 class StateMachine:
-    def __init__(self):
+    def __init__(self, boy):
+        self.boy = boy
         self.cur_state = Idle
 
     def start(self):
-        self.cur_state.enter()
+        self.cur_state.enter(self.boy)
 
     def update(self):
-        self.cur_state.do()
+        self.cur_state.do(self.boy)
 
     def draw(self):
-        self.cur_state.draw()
-
-
-
+        self.cur_state.draw(self.boy)
 
 
 class Boy:
@@ -43,7 +41,7 @@ class Boy:
         self.frame = 0
         self.action = 3
         self.image = load_image('animation_sheet.png')
-        self.state_machine = StateMachine()
+        self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def update(self):
