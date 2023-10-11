64e5573b073172546c2e97c4a21722d75f4426ae
Hyno03 <jenny2117@naver.com>
2023-10-11 22:04:51 +0900

    Boy runs for 5 second and run only until it goes beyond the screen

--- boy.py
+++ boy.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, SDL_KEYDOWN, SDLK_a
+from pico2d import load_image, SDL_KEYDOWN, SDLK_a, get_time
 
 
 def a_down(e):
@@ -12,11 +12,15 @@ def time_out(e):
 class Idle:
 
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
         pass
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
 
     @staticmethod
@@ -29,20 +33,23 @@ class Idle:
 
 
 class AutoRun:
-
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
         boy.action = 1
+        boy.run_time = get_time()
         pass
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
 
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        boy.x += 10
+        if boy.x < 700:
+            boy.x += 10
+        if get_time() - boy.run_time > 2:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(boy):
@@ -59,7 +66,7 @@ class StateMachine:
         }
 
     def start(self):
-        self.cur_state.enter(self.boy)
+        self.cur_state.enter(self.boy, ('NONE', 0))
 
     def update(self):
         self.cur_state.do(self.boy)
@@ -67,9 +74,9 @@ class StateMachine:
     def handle_event(self, e):
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
-                self.cur_state.exit(self.boy)
+                self.cur_state.exit(self.boy, e)
                 self.cur_state = next_state
-                self.cur_state.enter(self.boy)
+                self.cur_state.enter(self.boy, e)
                 return True
         return False
 
