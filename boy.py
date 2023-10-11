31e6c5daa9a523c7e9acb91098df422d1f1ca486
Hyno03 <jenny2117@naver.com>
2023-10-11 20:54:33 +0900

    Boy auto run when 'a' key down

--- boy.py
+++ boy.py
@@ -1,4 +1,12 @@
-from pico2d import load_image
+from pico2d import load_image, SDL_KEYDOWN, SDLK_a
+
+
+def a_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
+
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
 
 
 class Idle:
@@ -20,16 +28,50 @@ class Idle:
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
 
+class AutoRun:
+
+    @staticmethod
+    def enter(boy):
+        boy.action = 1
+        pass
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += 10
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
 class StateMachine:
     def __init__(self, boy):
-        self.boy = boy
         self.cur_state = Idle
+        self.boy = boy
+        self.transitions = {
+            Idle: {a_down: AutoRun},
+            AutoRun: {time_out: Idle}
+        }
 
     def start(self):
         self.cur_state.enter(self.boy)
 
     def update(self):
         self.cur_state.do(self.boy)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.boy)
+                self.cur_state = next_state
+                self.cur_state.enter(self.boy)
+                return True
+        return False
 
     def draw(self):
         self.cur_state.draw(self.boy)
@@ -48,6 +90,7 @@ class Boy:
         self.state_machine.update()
 
     def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
         pass
 
     def draw(self):
